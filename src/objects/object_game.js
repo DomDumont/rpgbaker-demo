37680484da3fb472cd421ab8cdbeefedd9720c1f
DomDom <dom.dumont@gmail.com>
2018-11-26 14:53:41 +0100

    start some grid stuff

--- src/objects/object_game.js
+++ src/objects/object_game.js
@@ -26,12 +26,16 @@ export default class ObjectGame extends GameObject {
 
     this.spawnRoom = -1
     this.doTransition = false
+
+    this.cellSize = 32
+
     super.Init()
   }
 
   Destroy () {
     console.log('ObjectGame Destroy')
     this.removeChild(this.fullScreenQuad)
+    this.removeChild(this.grid)
   }
   Update (delta) {
     if (this.room.game.input.IsKeyPressed(this.room.game.input.vk_d)) {
@@ -42,6 +46,8 @@ export default class ObjectGame extends GameObject {
         console.log(obj.name)
         obj.graphicsHitArea.alpha = newAlpha
       })
+
+      this.grid.alpha = newAlpha
     }
 
     this.fullScreenQuad.x = -this.room.game.app.stage.position.x
@@ -62,5 +68,38 @@ export default class ObjectGame extends GameObject {
       }
     }
     super.Update(delta)
+  }
+
+  OnRoomStart () {
+    console.log('OnRoomStart')
+
+    this.roomWidth = this.room.game.GetCurrentRoomWidth()
+    this.roomHeight = this.room.game.GetCurrentRoomHeight()
+
+    console.log('GetCurrentRoom ' + this.roomWidth + ' ' + this.roomHeight)
+
+    if (this.grid) {
+      this.removeChild(this.grid)
+    }
+
+    this.grid = new PIXI.Graphics()
+    this.grid.lineStyle(1, 0x00ffee, 0.5)
+
+    let spaceWidth = Math.floor(this.roomWidth / this.cellSize)
+    for (let i = 0; i < spaceWidth; i++) {
+      this.grid.moveTo(i * this.cellSize, 0)
+      this.grid.lineTo(i * this.cellSize, this.roomHeight)
+    }
+
+    let spaceHeight = Math.floor(this.roomHeight / this.cellSize)
+    for (let j = 0; j < spaceHeight; j++) {
+      this.grid.moveTo(0, j * this.cellSize)
+      this.grid.lineTo(this.roomWidth, j * this.cellSize)
+    }
+
+    this.grid.alpha = 0
+    this.addChild(this.grid)
+
+    this.grid.parentGroup = this.room.game.groups.get('2')
   }
 }
